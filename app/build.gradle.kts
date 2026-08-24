766a4457171ad28acc885daebb08111bc3b0d091
Khang Tran <khangtran@uney.com>
2026-08-24 14:48:09 +0700

    chore: release v1.0.57 [skip apk deploy]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.vpcoffee"
         minSdk = 26
         targetSdk = 37
-        versionCode = 55
+        versionCode = 56
         versionName = "1.0.46"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
