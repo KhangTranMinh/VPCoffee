5e9530bf1144d27403e4a35e80df731d7e8ec2e6
Khang Tran <khangtran@uney.com>
2026-08-25 10:10:32 +0700

    chore: release pushtest v1.0.6 [skip apk deploy]

--- pushtest/build.gradle.kts
+++ pushtest/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.vpcoffee.pushtest"
         minSdk = 26
         targetSdk = 37
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.0"
     }
 
